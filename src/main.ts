cf257ad6b6196e9342c6c974a7a97f97d9914dbf
hossein <hosseinfalah2021@gmail.com>
2025-03-19 09:04:44 +0330

    docs(swagger): add API documentation for authentication module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerConfigInit } from './configs/swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  SwaggerConfigInit(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
