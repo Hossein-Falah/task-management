c70d78d094c58abf07429db81e1c88a0dbefd618
hossein <hosseinfalah2021@gmail.com>
2025-03-19 13:47:02 +0330

    feat(user): create user entity and database table

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerConfigInit } from './configs/swagger.config';
 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService)
   SwaggerConfigInit(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get<number>("APP_PORT") ?? 3000, () => {
+    console.log(`Server is running or http://localhost:${configService.get<number>("APP_PORT")}`);
+    console.log(`Swagger is running http://localhost:${configService.get<number>("APP_PORT")}${configService.get<string>("DOC_PATH")}`);
+  });
 }
 bootstrap();
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
+import { EntityNames } from "src/common/enum/entity.enum";
+import { AbstractEntity } from "src/common/abstracts/abstract.entity";
+import { UserRole } from "src/common/enum/role.status";
+
+@Entity(EntityNames.User)
+export class UserEntity extends AbstractEntity {
+    @Column({ nullable: true, unique: true })
+    phone:string;
+    @Column({ nullable: true, unique: true })
+    username:string;
+    @Column({ nullable: true, unique: true })
+    email:string;
+    @Column()
+    password:string;
+    @Column({ default: UserRole.Admin })
+    role:string;
+    @CreateDateColumn()
+    createdAt:Date;
+    @UpdateDateColumn()
+    updatedAt:Date;
+}
